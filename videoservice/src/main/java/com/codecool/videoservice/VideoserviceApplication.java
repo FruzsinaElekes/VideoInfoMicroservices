35465cdb5c73f9168a3caa6c229de48353ff1d0f
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-26 20:17:21 +0100

    Add loadBalanced RestTemplate, fix recommendation.url in properties

--- videoservice/src/main/java/com/codecool/videoservice/VideoserviceApplication.java
+++ videoservice/src/main/java/com/codecool/videoservice/VideoserviceApplication.java
@@ -5,7 +5,10 @@ import com.codecool.videoservice.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -46,5 +49,11 @@ public class VideoserviceApplication {
                         .build()
                 );
         videoRepository.saveAll(videos);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
