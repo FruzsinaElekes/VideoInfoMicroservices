8ec9d80bb6690e792eb04ca3fceb73eb78abfe59
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-26 18:06:25 +0100

    Add possibility to save & retrieve recommendations

---
+++ recommendation/src/main/java/com/codecool/recommendation/controller/Controller.java
@@ -0,0 +1,26 @@
+package com.codecool.recommendation.controller;
+
+import com.codecool.recommendation.entity.Recommendation;
+import com.codecool.recommendation.service.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recommendation")
+public class Controller {
+
+    @Autowired
+    private RecommendationService recommendationService;
+
+    @GetMapping("/{videoId}")
+    public List<Recommendation> getRecommendationsForVideo(@PathVariable long videoId) {
+        return recommendationService.getAllRecommendationsForVideo(videoId);
+    }
+
+    @PostMapping("/new")
+    public Recommendation postRecommendationForVideo(@RequestBody Recommendation toSave) {
+        return recommendationService.saveNewRecommendation(toSave);
+    }
+}
---
+++ recommendation/src/main/java/com/codecool/recommendation/entity/Recommendation.java
@@ -0,0 +1,22 @@
+package com.codecool.recommendation.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Recommendation {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private int rating;
+    private String comment;
+    private long video;
+}
