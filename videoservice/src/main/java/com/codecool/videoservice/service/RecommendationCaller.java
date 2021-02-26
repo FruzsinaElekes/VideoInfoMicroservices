ec75c8e975fd00e5a31c7379ea928325ca15e230
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-26 20:18:26 +0100

    Call recommendation service, add RecomResult model for return type

---
+++ videoservice/src/main/java/com/codecool/videoservice/service/RecommendationCaller.java
@@ -0,0 +1,28 @@
+package com.codecool.videoservice.service;
+
+import com.codecool.videoservice.model.RecomResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Set;
+
+@Service
+public class RecommendationCaller {
+
+    @Value("${recommendation.url}")
+    private String baseUrl;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public Set<RecomResult> getRecommendationsForVideo(long video) {
+        String url = String.format(baseUrl + "/get/%s", video);
+        ResponseEntity<RecomResult[]> entity = restTemplate.getForEntity(url, RecomResult[].class);
+        RecomResult[] results = entity.getBody();
+        return Set.of(results);
+    }
+
+}
