c8d16366eec39792d84780ed129234a9c1d85940
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-28 10:33:16 +0100

    Add routes to App.js, Home component fetches video data without recommendations

--- video_info/src/App.js
+++ video_info/src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Home from './Home';
+import DetailedPage from './DetailedPage';
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} ></Route>
+        <Route path="/video/:id" component={DetailedPage}></Route>
+      </div>
+    </Router>
   );
 }
 
--- video_info/src/Home.js
+++ video_info/src/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import VideoItem from './VideoItem';
 
 export default function Home() {
+
+    const [videoList, setVideoList] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:8762/videoservice/video/list")
+            .then(response => setVideoList(response.data))
+    }, [])
+
     return (
         <div>
-            This is the top part of the page
+            <h2>This is the top part of the page</h2>
+            <div>
+                {videoList && videoList.map(i => <VideoItem key={i.id} base = {i}/>)}
+            </div>
         </div>
     )
 }
