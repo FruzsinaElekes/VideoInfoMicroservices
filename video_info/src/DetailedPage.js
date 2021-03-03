c2867af4c9b0e4859c17f8623d5a41643ce6bc7a
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-03 11:17:30 +0100

    Add form to submit recommendation

--- video_info/src/DetailedPage.js
+++ video_info/src/DetailedPage.js
@@ -3,16 +3,20 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Comment from './Comment';
 import EmbeddedVideo from './EmbeddedVideo';
+import Form from "./Form";
+import Button from '@material-ui/core/Button';
 
 export default function DetailedPage() {
     
     const [details, setDetails] = useState(undefined)
     const {id} = useParams()
+    const [showForm, setShowForm] = useState(false)
+    const [updater, setUpdater] = useState(false)
 
     useEffect(() => {
         axios.get(`http://localhost:8762/videoservice/video/${id}`)
         .then(response => setDetails(response.data))
-    }, [])
+    }, [updater])
 
     return (
         <div>
@@ -24,6 +28,9 @@ export default function DetailedPage() {
                 <div>
                     {details.recommendations.map(r => <Comment key={r.id} recommendation={r}></Comment>)}
                 </div>
+                {!showForm && <Button variant="contained" color="primary" onClick={() => setShowForm(true)}>Add recommendation</Button>}
+                {showForm && <Form setUpdater={setUpdater} videoId={id} setShowForm={setShowForm} ></Form>}
+
             </React.Fragment>
             : <React.Fragment>Still loading data</React.Fragment>
             }
---
+++ video_info/src/Form.js
@@ -0,0 +1,52 @@
+import React, { useRef } from 'react';
+import Button from '@material-ui/core/Button';
+import { TextField } from '@material-ui/core';
+import styled from 'styled-components';
+import axios from 'axios'
+
+
+export default function Form(props) {
+
+    const commentRef = useRef()
+    const ratingRef = useRef()
+
+    const submit = () => {
+        const body = {
+            videoId : props.videoId,
+            comment: commentRef.current.value,
+            rating: ratingRef.current.value
+        }
+        axios({
+            method: 'POST',
+            url: `http://localhost:8762/videoservice/video/${props.videoId}/recommendation`, 
+            data: body,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.status === 200){
+                props.setShowForm(false)
+                props.setUpdater(prev => !prev)
+            }
+            console.log(response)})
+    }
+
+    return (
+        <FormContainer>
+            <TextField inputRef={ratingRef} label="Rating" type="number" inputProps={{min : 1, max : 5}}></TextField>
+            <TextField inputRef={commentRef} label="Comment" multiline rows={4}></TextField>
+            <div>
+                <Button variant="contained" color="primary" width="100px" onClick={() => props.setShowForm(false)}>Cancel</Button>
+                <Button variant="contained" color="secondary" onClick={submit}>Submit</Button>
+            </div>
+        </FormContainer>
+    )
+}
+
+const FormContainer = styled.div`
+    margin: auto;
+    width: 300px;
+    display: flex;
+    flex-direction: column;
+`
